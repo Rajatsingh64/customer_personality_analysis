202fd5b8441c2b2f33a8b5ea9730ca8981789c25
RJSINGH64 <rajat.k.singh64@gmail.com>
2025-03-10 18:35:17 +0530

    apache-airflow dag update

--- airflow/dags/clustering_pipeline.py
+++ airflow/dags/clustering_pipeline.py
@@ -1,4 +1,3 @@
-from asyncio import tasks
 import json
 from textwrap import dedent
 import pendulum
@@ -6,11 +5,9 @@ import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-
 with DAG(
-    'Customer Personality',
+    'Customer_Personality',
     default_args={'retries': 2},
-    # [END default_args]
     description='Customer Personality Analysis',
     schedule_interval="@weekly",
     start_date=pendulum.datetime(2025, 3, 10, tz="UTC"),
@@ -18,43 +15,35 @@ with DAG(
     tags=['example'],
 ) as dag:
 
-    
     def download_files(**kwargs):
         bucket_name = os.getenv("BUCKET_NAME")
         input_dir = "/app/input_files"
-        #creating directory
-        os.makedirs(input_dir,exist_ok=True)
-        os.system(f"aws s3 sync s3://{bucket_name}/input_files /app/input_files")
+        os.makedirs(input_dir, exist_ok=True)
+        os.system(f"aws s3 sync s3://{bucket_name}/input_files {input_dir}")
 
     def cluster_prediction(**kwargs):
         from src.pipeline.clustering_pipeline import start_Cluster_prediction
         input_dir = "/app/input_files"
         for file_name in os.listdir(input_dir):
-            #make prediction
-            start_Cluster_prediction(input_file_path=os.path.join(input_dir,file_name))
-    
+            start_Cluster_prediction(input_file_path=os.path.join(input_dir, file_name))
+
     def sync_prediction_dir_to_s3_bucket(**kwargs):
         bucket_name = os.getenv("BUCKET_NAME")
-        #upload prediction folder to predictionfiles folder in s3 bucket
         os.system(f"aws s3 sync /app/prediction s3://{bucket_name}/prediction_files")
-    
 
-    download_input_files  = PythonOperator(
-            task_id="download_file",
-            python_callable=download_files
-
+    download_input_files = PythonOperator(
+        task_id="download_files",
+        python_callable=download_files
     )
 
     generate_prediction_files = PythonOperator(
-            task_id="prediction",
-            python_callable=cluster_prediction
-
+        task_id="generate_predictions",
+        python_callable=cluster_prediction
     )
 
     upload_prediction_files = PythonOperator(
-            task_id="upload_prediction_files",
-            python_callable=sync_prediction_dir_to_s3_bucket
-
+        task_id="upload_predictions",
+        python_callable=sync_prediction_dir_to_s3_bucket
     )
 
     download_input_files >> generate_prediction_files >> upload_prediction_files
--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -1,16 +1,12 @@
-from asyncio import tasks
 import json
-from textwrap import dedent
 import pendulum
 import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-
 with DAG(
-    'Customer Personality',
+    'Customer_Personality',
     default_args={'retries': 2},
-    # [END default_args]
     description='Customer Personality Analysis',
     schedule_interval="@weekly",
     start_date=pendulum.datetime(2025, 3, 10, tz="UTC"),
@@ -18,26 +14,29 @@ with DAG(
     tags=['example'],
 ) as dag:
 
-    
     def training(**kwargs):
+        """Runs the training pipeline"""
         from src.pipeline.training_pipeline import run_training_pipeline
         run_training_pipeline()
-    
+
     def sync_artifact_to_s3_bucket(**kwargs):
+        """Syncs artifact and model files to S3 bucket"""
         bucket_name = os.getenv("BUCKET_NAME")
+        
+        if not bucket_name:
+            raise ValueError("Environment variable 'BUCKET_NAME' is not set!")
+
         os.system(f"aws s3 sync /app/artifact s3://{bucket_name}/artifacts")
         os.system(f"aws s3 sync /app/saved_models s3://{bucket_name}/saved_models")
 
-    training_pipeline  = PythonOperator(
-            task_id="train_pipeline",
-            python_callable=training
-
+    training_pipeline = PythonOperator(
+        task_id="training_pipeline",
+        python_callable=training
     )
 
     sync_data_to_s3 = PythonOperator(
-            task_id="sync_data_to_s3",
-            python_callable=sync_artifact_to_s3_bucket
-
+        task_id="sync_data_to_s3",
+        python_callable=sync_artifact_to_s3_bucket
     )
 
     training_pipeline >> sync_data_to_s3
